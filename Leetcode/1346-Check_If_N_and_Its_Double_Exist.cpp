891dfdd43fb2c915b02d990679205ca68c9dfc91
Adam-Isaak-dev <67387234+Adam-Isaak-dev@users.noreply.github.com>
2022-09-30 11:43:59 -0500

    Leetcode - 1346. Check if N and It's Double Exist (O(n) and O(n)^2)

---
+++ Leetcode/1346-Check_If_N_and_Its_Double_Exist.cpp
@@ -0,0 +1,53 @@
+/*
+Given an array arr of integers, check if there exist two indices i and j such that :
+
+i != j
+0 <= i, j < arr.length
+arr[i] == 2 * arr[j]
+*/
+
+/* Testcases
+[10,2,5,3]
+[3,1,7,11]
+[-2,0,10,-19,4,6,-8]
+[-20,8,-6,-14,0,-19,14,4]
+[0,0]
+*/
+
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+// Two pass solution
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> uset;
+        int size = arr.size();
+        for (int num : arr) {
+          if(uset.count(2 * num) > 0 || uset.count(num/2) > 0 && (num % 2 != 1)) {
+            return true;
+          }
+          uset.insert(num);
+        }
+
+        return false;
+    }
+};
+
+// O(n)^2 solution
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        int size = arr.size();
+        for (int i = 0; i < size - 1; i++) {
+          for (int j = i + 1; j < size; j++) {
+              if(arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i]) {
+                return true;
+              }
+          }
+        }
+
+        return false;
+    }
+};
